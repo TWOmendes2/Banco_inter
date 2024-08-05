4cdc2eae19b87ae214d4c8304c4e93b482239ef4
Felipe Mendes <88950587+TWOmendes2@users.noreply.github.com>
2024-08-05 11:16:00 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var movingImage = document.getElementById('movingImage');
+    
+    // Inicia a animação ao carregar a página
+    movingImage.style.transform = "translateX(0)";
+});
